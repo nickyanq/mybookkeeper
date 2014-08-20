744c26c073c4fb3e69dc556de968e6189cb9c40b
corneliu iancu <corneliu.iancu@opti.ro>
2014-08-20 18:13:12 +0300

    update

--- app/app.js
+++ app/app.js
@@ -16,27 +16,26 @@ app.config(function($routeProvider, $locationProvider, $provide, $httpProvider) 
 				templateUrl: 'app/views/home.html',
 				controller: 'homeController',
 				resolve: {
-
 				}
 			})
 			.when('/register', {
 				templateUrl: 'app/views/register.html',
 				controller: 'registerController',
 				resolve: {
-					
 				}
 			})
 			.when('/login', {
 				templateUrl: 'app/views/login.html',
 				controller: 'loginController',
 				resolve: {
-					
 				}
 			})
 
 			.otherwise({redirectTo: '/'});
-	
-			$locationProvider.html5Mode(true);
+
+	$locationProvider.html5Mode(true);
+
+
 });
 
 
@@ -46,14 +45,16 @@ app.config(function($routeProvider, $locationProvider, $provide, $httpProvider) 
  *  - on route change success it hides the loading screen (via loadingView)
  *	- on route change error it displays a message in the console.
  */
-app.run(['$rootScope', '$q', '$timeout',
-	function($rootScope, $q, $timeout) {
-
+app.run(['$rootScope', '$templateCache', '$location', '$q', '$timeout',
+	function($rootScope, $templateCache, $location, $q, $timeout) {
+		$rootScope.$on('$viewContentLoaded', function() {
+			$templateCache.removeAll();
+		});
 
 		if (navigator.cookieEnabled === false) {
 			//no cookies enabled.
-			
-		} 
+
+		}
 
 		$rootScope.$on('$routeChangeStart', function(e, curr, prev) {
 			if (curr.$$route && curr.$$route.resolve) {
@@ -65,9 +66,15 @@ app.run(['$rootScope', '$q', '$timeout',
 		$rootScope.$on('$routeChangeSuccess', function(e, curr, prev) {
 			// Hide loading message
 			$rootScope.loadingView = false;
+			$('#left-panel').panel("close");
 			console.log('Route changed successfully.');
 		});
 		$rootScope.$on("$routeChangeError", function(e, curr, prev, rejection) {
 			console.log("ROUTE CHANGE ERROR: " + rejection);
 		});
+
+		$rootScope.redirect = function(url) {
+			$location.path(url);
+		}
+
 	}]);
